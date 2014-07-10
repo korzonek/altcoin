fbe6b3c7cf836412d958363fda8e028c7d0ed8c1
Carlos Cobo <toqueteos@gmail.com>
2014-07-10 04:15:07 +0200

    Move PoW into its own file.

--- miner/miner.go
+++ miner/miner.go
@@ -1,12 +1,7 @@
 package miner
 
 import (
-	"errors"
-	"math/big"
-	"math/rand"
 	"time"
-
-	"github.com/toqueteos/altcoin/tools"
 	"github.com/toqueteos/altcoin/types"
 )
 
@@ -55,52 +50,4 @@ func Miner(worker *Worker) {
 			need_new_work = true
 		}
 	}
-}
-
-// Proof-of-Work
-func PoW(block *types.Block, hashes int, restart chan bool) (bool, error) {
-	hh := tools.DetHash(block)
-	block.Nonce = randomNonce("100000000000000000")
-
-	// count = 0
-	var count int
-	for tools.DetHash(&types.HalfWay{Nonce: block.Nonce, HalfHash: hh}) > block.Target {
-		select {
-		case <-restart:
-			// return {"solution_found": true}
-			return true, nil
-		default:
-			count++
-			plus1(block.Nonce) // block.Nonce++
-
-			if count > hashes {
-				// return {"error": false}
-				return false, errors.New("POW error")
-			}
-
-			// For testing sudden loss in hashpower from miners.
-			// if block.Length > 150 {
-			// } else {
-			//     time.Sleep(10 * time.Millisecond) // 0.01 seconds
-			// }
-		}
-	}
-
-	return false, nil
-}
-
-var one = big.NewInt(1)
-
-func plus1(n *big.Int) {
-	n.Add(n, one)
-}
-
-func randomNonce(upto string) *big.Int {
-	upperBound := new(big.Int)
-	upperBound.SetString(upto, 10)
-
-	nonce := new(big.Int)
-	nonce.Rand(rand.New(rand.NewSource(99)), upperBound)
-
-	return nonce
 }
